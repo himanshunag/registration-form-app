35f29dadc1cca26bf54cfd7f7d2b2d4b8ce41248
himanshunag <himanshu.naag165@gmail.com>
2025-07-04 19:35:06 +0530

    Initial code

--- src/Home.py
+++ src/Home.py
@@ -41,24 +41,30 @@ st.markdown(
 registration_links = {
     "Dealer": {
         "link": "https://forms.gle/2AzUDGpt4BbhpJbp8",
-        "image": "dealer.png"  # Place dealer.png in the same folder
+        "image": "./dealer.png"  # Place dealer.png in the same folder
     },
     "Welder": {
         "link": "https://forms.gle/LQWZ7F3YJ47Cw5X46",
-        "image": "welder.png"  # Place welder.png in the same folder
+        "image": "./welder.png"  # Place welder.png in the same folder
     },
     "Painter": {
         "link": "https://forms.gle/cqLpi7tVBSgwSoJz7",
-        "image": "painter.png"  # Place painter.png in the same folder
+        "image": "./painter.png"  # Place painter.png in the same folder
     }
 }
 
 # Bold select label
-st.markdown('<span class="bold-label">Select the type of registration:</span>', unsafe_allow_html=True)
+st.markdown(
+    '<div style="display: flex; flex-direction: column; align-items: center;">'
+    '<span class="bold-label">Select the type of registration:</span>',
+    unsafe_allow_html=True
+)
 registration_type = st.selectbox(
     "",
-    ["Select"] + list(registration_links.keys())
+    ["Select"] + list(registration_links.keys()),
+    key="registration_type"
 )
+st.markdown('</div>', unsafe_allow_html=True)
 
 if registration_type != "Select":
     url = registration_links[registration_type]["link"]
